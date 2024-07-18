38f58a26333411f0f33625c33efc59e27286da1a
OneTwoWon <148797020+OneTwoWon@users.noreply.github.com>
2024-07-18 17:45:46 +0200

    EAS project

---
+++ eas.js
@@ -0,0 +1,11 @@
+const container = document.querySelector("container")
+const content = document.createElement("div")
+content.classicList.add("grid")
+content.textContent= "this is one";
+function createGrid(){
+    let num = prompt("Grid Size :");
+
+    for (let i = 0; 0 <= num; i++){
+    container.appendChild(content)
+}
+}
