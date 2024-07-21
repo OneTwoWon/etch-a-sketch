1fbc595eed7b396e3de8abd4c46681bedbbc801b
OneTwoWon <148797020+OneTwoWon@users.noreply.github.com>
2024-07-21 10:14:09 +0200

    added non interactive grid

--- eas.js
+++ eas.js
@@ -1,11 +1,24 @@
-const container = document.querySelector("container")
-const content = document.createElement("div")
-content.classicList.add("grid")
-content.textContent= "this is one";
-function createGrid(){
-    let num = prompt("Grid Size :");
+const container = document.querySelector("#container");
 
-    for (let i = 0; 0 <= num; i++){
+createGrid()
+
+function createGrid() {
+
+    //const input = document.createElement(in)
+    const numRows = 30; // Number of rows (or columns) in the grid
+    const num = numRows * numRows; // Total number of squares
+    const containerSize = 600; // Container width (fixed size for this example)
+    
+    // Calculate the size of each square
+    const squareSize = containerSize / numRows;
+    //prompt("Grid Size :");
+    for (let i = 0; i < num; i++){
+    const content = document.createElement("div")
+    content.classList.add("content");
+    content.style.boxSizing = "border-box"
+    content.style.border = "1px solid lightgray"
+    content.style.width = `${squareSize}px`
+    content.style.height = `${squareSize}px`
     container.appendChild(content)
 }
 }
